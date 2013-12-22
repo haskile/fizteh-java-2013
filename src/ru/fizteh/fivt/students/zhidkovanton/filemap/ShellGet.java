77b1a5a9824f42cafb9b61f81a0eb7080c5be1f8
goalkick <goalkick94@gmail.com>
2013-12-22 21:41:48 +0400

    fileMap

---
+++ src/ru/fizteh/fivt/students/zhidkovanton/filemap/ShellGet.java
@@ -0,0 +1,39 @@
+package ru.fizteh.fivt.students.zhidkovanton.filemap;
+
+import ru.fizteh.fivt.students.zhidkovanton.shell.BaseShellCommand;
+import ru.fizteh.fivt.students.zhidkovanton.shell.Command;
+import ru.fizteh.fivt.students.zhidkovanton.shell.InvalidCommandException;
+
+
+public final class ShellGet extends BaseShellCommand {
+    private State state;
+    public ShellGet(State state) {
+        setName("get");
+        setNumberOfArgs(2);
+        setHint("usage: get <key>");
+        this.state = state;
+    }
+
+    @Override
+    public void execute() {
+        String oldValue = state.get(getArg(1));
+        if (oldValue == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("found");
+            System.out.println(oldValue);
+        }
+    }
+
+    @Override
+    public boolean isAvaliableCommand(final Command command) {
+        if (name.equals(command.getArg(0))) {
+            if (command.length() < numberOfArgs) {
+                throw new InvalidCommandException(name + " " + hint);
+            }
+            args = command;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/ru/fizteh/fivt/students/zhidkovanton/filemap/ShellRemove.java
@@ -0,0 +1,37 @@
+package ru.fizteh.fivt.students.zhidkovanton.filemap;
+
+import ru.fizteh.fivt.students.zhidkovanton.shell.BaseShellCommand;
+import ru.fizteh.fivt.students.zhidkovanton.shell.Command;
+import ru.fizteh.fivt.students.zhidkovanton.shell.InvalidCommandException;
+
+public class ShellRemove extends BaseShellCommand{
+    private State state;
+    public ShellRemove(State state) {
+        setName("remove");
+        setNumberOfArgs(2);
+        setHint("usage: remove <key>");
+        this.state = state;
+    }
+
+    @Override
+    public void execute() {
+        String oldValue = state.remove(getArg(1));
+        if (oldValue == null) {
+            System.out.println("not found");
+        } else {
+            System.out.println("removed");
+        }
+    }
+
+    @Override
+    public boolean isAvaliableCommand(final Command command) {
+        if (name.equals(command.getArg(0))) {
+            if (command.length() < numberOfArgs) {
+                throw new InvalidCommandException(name + " " + hint);
+            }
+            args = command;
+            return true;
+        }
+        return false;
+    }
+}

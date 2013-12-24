ad9b9ff26460137e7358e35cda985f84ae173681
goalkick <goalkick94@gmail.com>
2013-12-24 06:38:32 +0400

    JUnit

--- src/ru/fizteh/fivt/students/zhidkovanton/JUnit/DataFactoryProvider.java
+++ src/ru/fizteh/fivt/students/zhidkovanton/JUnit/DataFactoryProvider.java
@@ -6,20 +6,20 @@ import java.io.File;
 
 public class DataFactoryProvider implements TableProviderFactory {
 
-    private void checkName(String name) {
-        if ((name == null) || name.trim().length() == 0) {
-            throw new IllegalArgumentException();
-        }
-
-        if (name.matches("[" + '"' + "'\\/:/*/?/</>/|/.\\\\]+") || name.contains(File.separator)
-                || name.contains(".")) {
-            throw new IllegalArgumentException();
-        }
-    }
-
     @Override
     public DataFactory create(String dir) {
-        checkName(dir);
+        if ((dir == null) || dir.trim().equals("")) {
+            throw new IllegalArgumentException();
+        }
+        File directoryFile = new File(dir);
+        if (!directoryFile.exists()){
+            if (!directoryFile.mkdir()){
+                throw new IllegalArgumentException();
+            }
+        }
+        if (!directoryFile.isDirectory()){
+            throw new IllegalArgumentException();
+        }
         return new DataFactory(dir);
     }
 }
